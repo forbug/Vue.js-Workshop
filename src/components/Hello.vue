38a7a133b06266bc64c7c216e2943870daac1c03
Alexander Schwartzberg <aeksco@gmail.com>
2018-02-03 11:36:46 -0500

    Added presenter notes, else

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,15 +1,155 @@
 <template>
   <div class="row">
-    <div class="col-lg-12 text-center">
+    <div class="col-lg-12">
 
-      <img src="@/assets/logo.png">
-
-      <h1>{{ msg }}</h1>
+      <h1>WELCOME</h1>
 
       <p class="subtitle">
         Built with Vue.js, Bootstrap, and FontAwesome
         <i class="fa fa-thumbs-up"></i>
       </p>
+
+      <!-- RENDER DATA -->
+      <p class='lead'>{{ msg }}</p>
+
+      <!-- DIRECTIVES -->
+      <!-- V-MODEL DIRECTIVE INPUT-->
+      <input type='text' class="form-control" v-model="msg" />
+
+      <!-- HANDLEBARS EXPRESSION -->
+      <p>
+        {{ checked ? 'Go' : 'Stop' }}
+      </p>
+
+      <!-- V-MODEL DIRECTIVE CHECKBOX -->
+      <input type="checkbox" v-model="checked">
+
+      <!-- V-IF DIRECTIVE -->
+      <i class='fa fa-check' v-if="checked"></i>
+
+      <!-- V-IF + expression -->
+      <i class='fa fa-check' v-if="checked"></i>
+      <i class='fa fa-times' v-if="!checked"></i>
+
+      <!-- V-ELSE DIRECTIVE -->
+      <!-- V-ELSE-IF is also allowed -->
+      <i class='fa fa-check' v-if="checked"></i>
+      <i class='fa fa-times' v-else></i>
+
+      <!-- V-BIND DIRECTIVE -->
+      <i v-bind:class="checked ? 'fa fa-check' : 'fa fa-times'"></i>
+
+      <br>
+
+      <!-- V-ON DIRECTIVE -->
+      <!-- Great for triggering an action that mutates the component's data -->
+      <!-- rather than having this element `MODEL` the data, like the v-model directive -->
+      <button class="btn btn-dark" v-on:click="checked = !checked">
+        <!-- STEP 1 -->
+        <i v-bind:class="checked ? 'fa fa-check' : 'fa fa-times'"></i>
+        <!-- STEP 2 -->
+        {{ checked ? 'Checked' : 'Unchecked' }}
+      </button>
+
+      <!-- REVIEW -->
+      <!-- QUESTIONS / TAKE 5 -->
+
+      <!-- V-FOR DIRECIVE - ITERATION -->
+      <ul class='list-group'>
+        <li class='list-group-item' v-for="each in todos" v-bind:key="each.label">
+
+          <!-- STEP 2 nested if -->
+          <!-- <i class='fa fa-check' v-if="each.done"></i> -->
+          <!-- <i class='fa fa-square-o' v-else></i> -->
+
+          <!-- STEP 2 - MORE ADVANCED -->
+          <!-- INTRODUCE COMPUTED SINCE THIS IS REPEATED LOGIC -->
+          <i v-bind:class="each.done ? 'fa fa-check' : 'fa fa-times'" v-on:click="each.done = !each.done"></i>
+
+          <!-- STEP 1 nested text -->
+          {{ each.label }}
+
+          <!-- STEP 3 - nested v-model -->
+          <input type="checkbox" v-model="each.done">
+
+        </li>
+      </ul>
+
+      <br>
+
+      <!-- LETS ADD AN INPUT AND BUTTON TO CREATE A NEW TODO -->
+      <!-- STEP 1 - ADD newTodo: to this.data -->
+      <div class="card card-body">
+        <label>New Todo</label>
+        <input type='text' class="form-control" v-model="newTodo"/>
+        <br>
+        <button class="btn btn-success" @click="addTodo">
+          <i class="fa fa-check"></i>
+          Add
+        </button>
+      </div>
+
+      <!-- REVIEW -->
+      <!-- We learned about the V-FOR DIRECTIVE -->
+      <!-- Used the v-model directive in conjunction with a v-on:click directive -->
+      <!-- to add a new TODO to the list -->
+
+      <!-- QUESTIONS -->
+
+      <!-- LET'S ADD A FUNCTION TO REMOVE A TODO -->
+      <!-- WIRE IT UP TO V-ON:CLICK DIRECIVE -->
+      <ul class='list-group'>
+        <li class='list-group-item' v-for="each in todos" v-bind:key="each.label">
+
+          <!-- STEP 2 nested if -->
+          <i class='fa fa-check' v-if="each.done"></i>
+          <i class='fa fa-square-o' v-else></i>
+
+          <!-- STEP 1 nested text -->
+          {{ each.label }}
+
+          <!-- STEP 3 - nested v-model -->
+          <input type="checkbox" v-model="each.done">
+
+          <!-- STEP 4 - INVOKE METHODS -->
+          <button class="btn btn-sm btn-danger pull-right" v-if="!each.editing" v-on:click="trashTodo(each)">
+            <i class="fa fa-trash"></i>
+          </button>
+
+          <!-- STEP 4 - EDIT METHOD -->
+          <button class="btn btn-sm btn-warning pull-right" v-if="!each.editing" v-on:click="editTodo(each)">
+            <i class="fa fa-pencil"></i>
+          </button>
+
+          <!-- Form -->
+          <input type="text" v-model="each.editing" v-if="each.editing">
+          <button class="btn btn-sm btn-success pull-right" v-if="each.editing" v-on:click="submitEdit(each)">
+            <i class="fa fa-check"></i>
+          </button>
+          <button class="btn btn-sm btn-dark pull-right" v-if="each.editing" v-on:click="cancelEdit(each)">
+            <i class="fa fa-times"></i>
+          </button>
+
+        </li>
+      </ul>
+
+      <!-- REVIEW -->
+      <!-- QUESTIONS -->
+
+      <!-- NEXT - INTRODUCE COMPONENTS -->
+      <!-- LETS ABSTRACT THE NEW FORM INTO A COMPONENT -->
+      <!-- 1 - NEW FILE -->
+      <!-- 2 - import component -->
+      <!-- 3 - setup components:{} -->
+      <!-- 4 - insert <TodoForm/> into template -->
+      <!-- 5 - but this isn't really wired up yet - introduce props -->
+
+      <!-- REVIEW -->
+      <!-- WRAP UP - WHAT'S NEXT -->
+      <!-- Adding more routes -->
+      <!-- Interfacing with APIs -->
+      <!-- Complex State Management -->
+      <!-- Advanced features - mixins, custom directives, slots, filters, server-side rendering -->
 
     </div>
   </div>
@@ -23,7 +163,34 @@ export default {
   name: 'Hello',
   data () {
     return {
-      msg: 'Welcome to the RCOS Vue.js Workshop'
+      msg: 'Welcome to Vue workshop!!!',
+      checked: true,
+      newTodo: '',
+      todos: [
+        { label: 'Study', done: true, editing: '' },
+        { label: 'Sleep', done: false, editing: '' },
+        { label: 'Eat', done: true, editing: '' },
+        { label: 'Code', done: true, editing: '' }
+      ]
+    }
+  },
+  methods: {
+    trashTodo (todo) {
+      this.todos = this.todos.filter(e => { return e.label !== todo.label })
+    },
+    addTodo () {
+      this.todos.push({ label: this.newTodo, done: false, editing: '' })
+      this.newTodo = ''
+    },
+    editTodo (todo) {
+      todo.editing = todo.label
+    },
+    submitEdit (todo) {
+      todo.label = todo.editing
+      todo.editing = ''
+    },
+    cancelEdit (todo) {
+      todo.editing = ''
     }
   }
 }
@@ -35,7 +202,7 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
   p.subtitle {
-    font-size: 2rem;
+    font-size: 1.25rem;
     color: #999999;
   }
 </style>
