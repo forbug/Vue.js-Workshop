036f42737bb627b687c073322d266d942f9bdbf6
Alexander Schwartzberg <aeksco@gmail.com>
2018-02-06 15:23:17 -0500

    Added Component.complete.vue reference files

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,26 +1,16 @@
 <template>
   <div class="row">
 
-    <!-- V-FOR -->
     <div class="col-lg-6">
-      <ul class="list-group">
-        <li :class='eachCss(each)' v-for="each in todos" :key="each.label">
-
-          <!-- todoIcon computed -->
-          <i v-bind:class="eachIcon(each)" v-on:click="each.done = !each.done"></i>
-
-          {{ each.label }}
-
-          <button class="btn btn-danger pull-right btn-sm" @click="removeTodo(each)">
-            <i class="fa fa-trash"></i>
-          </button>
-
-        </li>
-      </ul>
+      <div class="card card-body">
+        <p class="lead">ToDo List Goes Here</p>
+      </div>
     </div>
 
     <div class="col-lg-6">
-      <Form v-bind:addTodo="add"/>
+      <div class="card card-body">
+        <p class="lead">Todo Form Goes Here</p>
+      </div>
     </div>
 
   </div>
@@ -30,13 +20,8 @@
 
 <script>
 
-import Form from './Form'
-
 export default {
   name: 'TodoList',
-  components: {
-    Form
-  },
   data () {
     return {
       todos: [
@@ -45,21 +30,6 @@ export default {
         { label: 'Breath', done: true },
         { label: 'Code', done: false }
       ]
-    }
-  },
-  methods: {
-    add (newTodo) {
-      this.todos.push({ label: newTodo, done: false })
-      this.newTodo = ''
-    },
-    removeTodo (todo) {
-      this.todos = this.todos.filter(e => { return e.label !== todo.label })
-    },
-    eachCss (each) {
-      return each.done ? 'list-group-item list-group-item-success' : 'list-group-item'
-    },
-    eachIcon (each) {
-      return each.done ? 'fa fa-check' : 'fa fa-square-o'
     }
   }
 }
